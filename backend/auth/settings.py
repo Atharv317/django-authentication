e45f2bf7f78fd3e4d10cdc5fcb92d2808f6e6549
Atharv317 <atharvshukla315@gmail.com>
2026-04-09 13:58:56 +0530

    Setting up Frontend and connection with Backend

--- backend/auth/settings.py
+++ backend/auth/settings.py
@@ -26,11 +26,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "users"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -38,6 +40,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
 ]
 
 ROOT_URLCONF = "auth.urls"
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from users.views import function
 
 urlpatterns = [
-   
+   path("",function,name="function")
 ]
